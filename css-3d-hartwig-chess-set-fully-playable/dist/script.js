56d1fa80cc46374c65fd89cfb525719e888e46b2
tylerbry <104282235+Tylerbryy@users.noreply.github.com>
2022-05-22 22:31:27 -0500

    Add files via upload

---
+++ css-3d-hartwig-chess-set-fully-playable/dist/script.js
@@ -0,0 +1,7 @@
+/*                                       
+
+I had some issues running the javascript directly into codepen,
+I wrapped everything in one external js file.
+The full code can be found here : https://github.com/Tylerbryy/3D-Hartwig-chess-set
+
+*/
